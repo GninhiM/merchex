18764d757414d293075ae8c2301cf43fa275dd9a
GninhiM <83226582+GninhiM@users.noreply.github.com>
2023-03-26 06:23:09 +0200

    ajout de la page de base pour la conception des gabarits

--- listings/views.py
+++ listings/views.py
@@ -6,20 +6,15 @@ from listings.models import Band, Listing
 def hello(request):
     bands = Band.objects.all()
     return render(request, 'listings/hello.html', 
-                  {'first_band': bands[4]})
+                  {'bands': bands})
 
 def about(request):
-    return HttpResponse('<h1> A propos </h1> <p> Nous adorons merch !</p>')
+    return render(request, 'listings/about.html')
 
 def listings(request):
-    titles =Listing.objects.all()
-    return HttpResponse(f"""<h1> Page de listing</h1>
-                        <p>Nous allons afficher la liste de nos produits sur cette page</>
-                        <ul>
-                            <li>{titles[0].title}</li>
-                            <li>{titles[1].title}</li>
-                            <li>{titles[2].title}</li>
-                        </ul> """)
-
+    titles = Listing.objects.all()
+    return render (request, "listings/listings.html", 
+                   {'titles': titles})
+              
 def contact(request):
-    return HttpResponse('<h1>Contactez nous</h1> <p>Formulaire de contact</p>')
+    return render(request, "listings/contact.html")
